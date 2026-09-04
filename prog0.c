0c63bf75ac122d5b0ad7ad65c865ed2df670e9e5
Jolie Goldstein <jolie.goldstein@lehigh.edu>
2026-09-04 18:22:01 -0400

    hexDigit and read_hex

--- prog0.c
+++ prog0.c
@@ -9,16 +9,36 @@ union value{
     float fval;
     unsigned char bytes[4];
 };
-// reads 8 hex characters from string input and stores it in the union v
-// returns -1 if the hexadecimal number is invalid, 0 otherwise
-int read_hex(union value *v, char *input);
-
 // converts the ASCII hex character c to binary
 // returns the hex value of c if c is a valid hex digit, -1 otherwise
-char hexDigit(char c);
+char hexDigit(char c) {
+    if(c >= '0' && c <= '9')
+        return c - '0';
+    if(c >= 'a' && c <= 'f')
+        return c - 'a' + 10;
+    if(c >= 'A' && c <= 'F')
+        return c - 'A' + 10;
+    return -1;
+}
+// reads 8 hex characters from string input and stores it in the union v
+// returns -1 if the hexadecimal number is invalid, 0 otherwise
+int read_hex(union value *v, char *input){
+    if(strlen(input) != 8)
+        return -1;
+    for(int i = 0; i < 4; i++){
+        char high = hexDigit(input[2*i]);
+        char low = hexDigit(input[2*i + 1]);
+        if(high == -1 || low == -1)
+            return -1;
+        v->bytes[3 - i] = (high << 4) | low; // 2 4 bit nibbles
+    }
+    return 0;
+}
 
 // returns true if x has any even bit equal to 1, 0 otherwise
-int any_even_one(unsigned x);
+int any_even_one(unsigned x){
+    return (x & 0x55555555) != 0;
+}
 // returns a mask indicating the position of the left most one in x
 int leftmost_one(unsigned x);
 // returns x shifted n positions to the left with the n most significant bits of x 
@@ -40,6 +60,21 @@ int main(int argc, char** argv){
     if(argc != 3 && argc != 4){
         printf("Invalid number of arguments");
         exit(0);
+    }
+    char *op = argv[1];
+    union value v;
+    if(strcmp(op, "even") == 0){
+        if(read_hex(&v, argv[2]) == -1){
+            printf("Invalid hex value");
+            exit(0);
+        }
+        if(any_even_one(v.uval))
+            printf("True");
+        else
+            printf("False");
+    }
+    else{
+        printf("Invalid operation");
     }
     return 0;
 }
